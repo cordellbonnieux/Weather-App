053e8940627e4d28c888354e116b0fdeead75de9
Cordell <cordellbonnieux@gmail.com>
2020-12-13 18:32:31 -0800

    Added OpenWeather API.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,17 @@
 import _ from "lodash";
 import "./style.css";
-import Kitty from "./kitty.jpg";
-
-function component() {
-  const element = document.createElement("div");
-
-  // Lodash, currently imported by this script
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
-
-  return element;
-}
-
-document.body.appendChild(component());
 
 const main = document.querySelector("main");
-function test() {
-  const box = new Image();
-  box.src = Kitty;
-  main.appendChild(box);
+
+async function getWeather(){
+  try {
+  const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Vancouver&appid=ebea71aaa693706b343e3ef899d424b3', {mode: 'cors'}) 
+  const data = await response.json()
+  console.log(data)
+  } catch (error){
+    console.error(error)
+  }
 }
-test();
+getWeather()
+
+
