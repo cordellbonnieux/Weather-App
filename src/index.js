373c67d9894d171001f65fc35dd5cb330ef32100
Cordell <cordellbonnieux@gmail.com>
2020-12-13 19:36:07 -0800

    Search form built.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import _ from "lodash";
 import "./style.css";
 
-const main = document.querySelector("main");
+const main = document.querySelector("main")
+const form = document.createElement('form')
 
-async function getWeather(){
+function createForm(){
+  const wrapper = document.createElement('div')
+    main.appendChild(wrapper)
+    wrapper.appendChild(form)
+  const search = document.createElement('input')
+    search.type = 'text'
+    form.appendChild(search)
+  const button = document.createElement('button')
+    button.type = 'submit'
+    button.textContent = 'search'
+    form.appendChild(button)
+}
+createForm()
+async function getWeather(query){
+  const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + query + '&appid=ebea71aaa693706b343e3ef899d424b3'
   try {
-  const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Vancouver&appid=ebea71aaa693706b343e3ef899d424b3', {mode: 'cors'}) 
-  const data = await response.json()
-  console.log(data)
+    const response = await fetch(url, {mode: 'cors'}) 
+    const data = await response.json()
+    console.log(data)
   } catch (error){
     console.error(error)
   }
 }
-getWeather()
-
-
