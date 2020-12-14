ebba74d8c22059508b6dafb8be8608d43bf28a6f
Cordell <cordellbonnieux@gmail.com>
2020-12-14 12:05:08 -0800

    Added the containers to display information, also styled a bit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import _ from "lodash" // do i really need it?
 import "./style.css"
 
+// global variables
 const main = document.querySelector("main")
 const form = document.createElement('form')
 
+// building blocks 
+function createHeader(){
+  const wrapper = document.createElement('div')
+    wrapper.setAttribute('id', 'headerWrapper')
+    main.appendChild(wrapper)
+  const title = document.createElement('h1')
+    title.textContent = 'Get The Weather!'
+    wrapper.appendChild(title)
+}
 function createForm(){
   const wrapper = document.createElement('div')
+    wrapper.setAttribute('id', 'formWrapper')
     main.appendChild(wrapper)
     form.setAttribute('name', 'form')
     form.setAttribute('onsubmit', 'return false')
@@ -20,7 +31,12 @@ function createForm(){
     button.textContent = 'search'
     form.appendChild(button)
 }
+
+// build the page
+createHeader()
 createForm()
+
+// weather retrieval
 async function getWeather(query){
   const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + query + '&appid=ebea71aaa693706b343e3ef899d424b3'
   try {
@@ -36,8 +52,6 @@ form.addEventListener('submit', function(){
     return false
   } else {
     const query = document.getElementById('textField').value
-    console.log('form submitted')
-    console.log(query)
-    //getWeather(query)
+    getWeather(query)
   }
 })
