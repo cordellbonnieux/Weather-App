d5713b17660cfb9516b44a657985f923e95e840c
Cordell <cordellbonnieux@gmail.com>
2020-12-26 20:09:57 -0800

    Moved the countrylist getting functions to build.js. Also added country list json file, which seems to be more efficient then finding an API to call.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {createHeader, createForm, resultsArea, createFooter} from './build'
+import {createHeader, createForm, resultsArea, createFooter, getCountries, countryPicker} from './build'
 import {getWeather, formTrigger, gatherWeather, displayWeather, defaultText, errorText} from './getWeather'
 import {getImage, startImage} from './getImage'
 
@@ -7,10 +7,12 @@ import {getImage, startImage} from './getImage'
 const main = document.getElementById("main")
 const form = document.createElement('form')
 const results = document.getElementById('results')
+//const picker = countryPicker()
+
 
 // build the page
 createHeader()
-createForm(main, form, '???')
+createForm(main, form)
 resultsArea(defaultText())
 createFooter()
 
@@ -22,31 +24,5 @@ startImage()
 
 import "./style.css"
 
-
-// this needs to be re-organized!
-
-export async function getCountries(){
-    try{
-        const response = await fetch('http://countryapi.gear.host/v1/Country/getCountries')
-        const data = await response.json()
-        console.log(data.Response)
-        return countryPicker(data.Response)
-    } catch(error){
-        console.error(error)
-    }
-}
-export function countryPicker(countryList){
-    const picker = document.createElement('select')
-        picker.setAttribute('id', 'countryPicker')
-        picker.setAttribute('name', 'country')
-        picker.textContent = 'country'
-    for (let i = 0; i < countryList.length; i++){
-        let option = document.createElement('option')
-            option.setAttribute('value', countryList[i].Alpha2Code)
-            option.textContent = countryList[i].Name
-        picker.appendChild(option)
-    }
-    return picker
-}
 
 
